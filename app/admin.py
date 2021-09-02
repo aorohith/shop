6253d859879c46039559307243154e1d081db767
ROHITH <aorohith@gmail.com>
2021-09-02 17:02:53 +0530

    Added link to view product and customer info to placed order section in admin panel using function in admin.py

--- app/admin.py
+++ app/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
+from django.utils.html import format_html
 from .models import *
+from django.urls import reverse
 
 # Register your models here.
 
@@ -18,4 +20,14 @@ class CartAdmin(admin.ModelAdmin):
 
 @admin.register(OrderPlaced)
 class OrderPlacedAdmin(admin.ModelAdmin):
-    list_display = ['id','user','customer','product','quantity','ordered_date','status']
+    list_display = ['id','user','customer','customer_info','product','product_info','quantity','ordered_date','status']
+
+    # link creation function to view details of customer in admin panel
+    def customer_info(self,obj):
+        link = reverse('admin:app_customer_change',args=[obj.customer.pk])
+        return format_html('<a href="{}">{}</a>', link, obj.customer.name)
+
+    # link creation function to view details of products in admin panel
+    def product_info(self,obj):
+        link = reverse('admin:app_product_change',args=[obj.product.pk])
+        return format_html('<a href="{}">{}</a>', link, obj.product.title)
